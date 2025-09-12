867b78a6ad363ba8c2476ee566eb6d0a0a94e6f2
jeff <jeffrey-31@hotmail.fr>
2025-09-12 08:58:48 +0200

    Create new type that accept two string to call them in new funtion

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -48,7 +48,7 @@ function isDirector(employee: Director | Teacher): employee is Director {
   return employee instanceof Director;
 }
 
-function executeWork(employee: Director | Teacher): any {
+function executeWork(employee: Director | Teacher): string {
   if (isDirector(employee)) {
     return employee.workDirectorTasks();
   }
@@ -57,3 +57,12 @@ function executeWork(employee: Director | Teacher): any {
 
 console.log(executeWork(createEmployee(200)));
 console.log(executeWork(createEmployee(1000)));
+
+type Subjects = `Math` | `History`;
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return `Teaching ${todayClass}`;
+  }
+  return `Teaching ${todayClass}`;
+}
